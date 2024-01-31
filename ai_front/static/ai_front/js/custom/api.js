3e7b503b52f3debaa0aa3b9493889da0f88fb4b4
myg813p <gijungcpy@gmail.com>
2024-01-31 17:41:09 +0900

    test

--- ai_front/static/ai_front/js/custom/api.js
+++ ai_front/static/ai_front/js/custom/api.js
@@ -41,7 +41,7 @@ async function handleSignin(){
         alert("회원가입이 완료되었습니다.\n로그인해주세요.")
         window.location.replace(loginPath);
     } else if(response.status == 403){
-        alert(`${response.status}\n아름넷에 등록되지 않은 사업자이거나\n사업자번호와 대표자가 일치하지 않습니다.\n사업자번호, 대표자명 확인 또는 관리자에게 문의하세요.`)
+        alert(`아름넷에 등록되지 않은 사업자이거나\n사업자번호와 대표자가 일치하지 않습니다.\n사업자번호, 대표자명 확인 또는 관리자에게 문의하세요.`)
     } else if(response.status == 400){
         if (response_json.message == 'username'){
             alert('이미 사용되는 아이디거나 입력되지 않았습니다.');
